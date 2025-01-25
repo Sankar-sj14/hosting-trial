9ef31ec892626735b48b982ca34eb068ee68b326
Sankar-sj14 <vsankarsamjose14@gmail.com>
2025-01-25 21:53:50 +0530

    stable1

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -3,8 +3,8 @@ import Accordions from './Accordions'
 
 const Faq = () => {
   return (
-    <div className='bg-slate-800 h-screen w-full p-24 flex flex-col justify-center items-center gap-14'>
-        <div className='text-white text-6xl w-full flex justify-start '>FAQ</div>
+    <div className='bg-slate-800 h-screen w-full p-4 sm:p-24 flex flex-col justify-center items-center gap-14'>
+        <div className='text-white text-xl font-bold sm:text-6xl w-full flex justify-start '>FAQ</div>
         <div className="sm:w-1/2 h-1/2 flex flex-col gap-4 w-full ">
         <Accordions/>
         <Accordions/>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,18 +5,19 @@ const Hero = () => {
   return (
     <div className='bg-slate-400 h-auto flex flex-col sm:flex-row'>
         <div className=' flex w-full sm:w-1/2 h-screen items-center justify-center bg-slate-200 p-4'>
-        <div className=' h-1/2 w-full flex flex-col justify-center items-center bg-slate-300 p-8'>
+        <div className=' h-1/2 w-full flex flex-col justify-center items-center bg-slate-300 p-8 gap-8'>
         <h1 className='text-xl text-white flex justify-center p-4'>Experience the Presence</h1>
-        <img src='/images/XP Icon.png' className='w-full p-4'></img>
-        <Button variant="outline" className="block">Explore</Button>
+        <img src='/images/XP Icon.png' className='w-full'></img>
+        <Button variant="outline" className="block mt-8">Explore</Button>
 
         </div>
         </div>
         <div className="h-screen w-full sm:w-1/2 py-24 flex items-center justify-center p-14">
-            <div className="h-1/2 xl:h-full w-full bg-slate-600 flex items-center justify-center text-white"><h1>Big image Here and testing</h1></div>
+            <div className="h-1/2 xl:h-full w-full bg-slate-600 flex items-center justify-center text-white"><h1>Image here</h1></div>
         </div>
     </div>
   )
 }
 
 export default Hero
+

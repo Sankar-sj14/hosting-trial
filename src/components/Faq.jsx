d9b74d304199cfd975acda7c48152ed9a333ace9
Sankar-sj14 <vsankarsamjose14@gmail.com>
2025-01-25 21:28:50 +0530

    adjust

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -5,7 +5,7 @@ const Faq = () => {
   return (
     <div className='bg-slate-800 h-screen w-full p-24 flex flex-col justify-center items-center gap-14'>
         <div className='text-white text-6xl w-full flex justify-start '>FAQ</div>
-        <div className="w-1/2 h-1/2 flex flex-col gap-4 ">
+        <div className="sm:w-1/2 h-1/2 flex flex-col gap-4 w-full ">
         <Accordions/>
         <Accordions/>
         <Accordions/>

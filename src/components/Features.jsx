0858378db6a411a9ce2f5d6d0035ac593ad338e3
Sankar-sj14 <vsankarsamjose14@gmail.com>
2025-01-25 23:17:57 +0530

    tab response

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Features = () => {
   return (
       <div className='bg-slate-800 h-screen w-full p-4 sm:p-24 flex flex-col justify-center gap-8 sm:gap-14'>
-        <div className='text-white text-xl font-bold sm:text-6xl'>FEATURES</div>
+        <div className='text-white text-xl font-bold sm:text-4xl'>FEATURES</div>
         <div className="w-full h-full gap-4 flex flex-col md:flex md:flex-row md:h-1/2 bg-slate-400 p-8">
         <div className=" h-1/4 bg-slate-600  w-full md:h-full rounded-md text-white flex justify-center items-center">Feature</div>
         <div className=" h-1/4 bg-slate-600  w-full md:h-full rounded-md text-white flex justify-center items-center">Feature</div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,9 @@ import { Button } from "@/components/ui/button"
 
 const Hero = () => {
   return (
-    <div className='bg-slate-400 h-auto flex flex-col sm:flex-row sm:p-24'>
+    <div className='bg-slate-400 h-auto flex flex-col sm:flex-row'>
         <div className=' flex w-full sm:w-1/2 h-screen items-center justify-center bg-slate-200 p-4'>
-        <div className=' h-1/2 w-full flex flex-col justify-center items-center bg-slate-300 p-8 gap-8'>
+        <div className=' h-1/2 w-full flex flex-col justify-center items-center bg-slate-300 p-14 gap-8'>
         <h1 className='text-xl text-white flex justify-center p-4'>Experience the Presence</h1>
         <img src='/images/XP Icon.png' className='w-full'></img>
         <Button variant="outline" className="block mt-8">Explore</Button>

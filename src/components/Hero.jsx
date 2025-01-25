f70a4140777ce389c9351a9f0af9c742e4bd24d8
Sankar-sj14 <vsankarsamjose14@gmail.com>
2025-01-25 13:08:09 +0530

    responsive update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,8 @@ import { Button } from "@/components/ui/button"
 
 const Hero = () => {
   return (
-    <div className='bg-slate-400 h-screen flex'>
-        <div className=' flex w-1/2 h-full items-center justify-center bg-slate-200 p-4'>
+    <div className='bg-slate-400 h-auto flex flex-col sm:flex-row'>
+        <div className=' flex w-full sm:w-1/2 h-screen items-center justify-center bg-slate-200 p-4'>
         <div className=' h-1/2 w-full flex flex-col justify-center items-center bg-slate-300 p-8'>
         <h1 className='text-xl text-white flex justify-center p-4'>Experience the Difference</h1>
         <img src='/images/XP Icon.png' className='w-full p-4'></img>
@@ -12,8 +12,8 @@ const Hero = () => {
 
         </div>
         </div>
-        <div className="h-full w-1/2 py-24 flex items-center justify-center p-14">
-            <div className="h-full w-full bg-slate-600 flex items-center justify-center text-white"><h1>Big image Here and testing</h1></div>
+        <div className="h-screen w-full sm:w-1/2 py-24 flex items-center justify-center p-14">
+            <div className="h-1/2 xl:h-full w-full bg-slate-600 flex items-center justify-center text-white"><h1>Big image Here and testing</h1></div>
         </div>
     </div>
   )

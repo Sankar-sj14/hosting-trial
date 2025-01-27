352b3e051f067c912b63fddf5a5c32d514ba9625
Sankar-sj14 <vsankarsamjose14@gmail.com>
2025-01-27 14:32:34 +0530

    Features with map.

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import CardGlow from './CardGlow'
 
 const Features = () => {
+
+  const Featurelist = ["Feature1","Feature2","Feature3","Feature4"];
+
   return (
       <div className='bg-gradient-to-b from-slate-800 via-black to-slate-800 h-screen w-full p-4 sm:p-24 flex flex-col justify-center gap-8 sm:gap-14 text-white'>
         <div className='text-white text-xl font-bold sm:text-4xl'>FEATURES</div>
         <div className="w-full h-full gap-4 flex flex-col md:flex md:flex-row md:h-1/2 bg-transparent p-8">
-        <CardGlow Feature={"Feature 1"}/>
-        <CardGlow Feature={"Feature 2"}/>
-        <CardGlow Feature={"Feature 3"}/>
-        <CardGlow Feature={"Feature 4"}/>
+        {
+          Featurelist.map((feature, index) => (
+            <CardGlow key={index} Feature={feature}/>
+              ))}
         </div>
         </div>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className='bg-gradient-to-b  from-slate-500 via-gray-800 to-slate-800 p-4 h-auto flex flex-col sm:flex-row'>
         <div className=' flex w-full sm:w-1/2 h-screen items-center justify-center '>
         <div className=' h-1/2 w-full flex flex-col justify-center items-center p-14 gap-8 '>
-        <h1><span className='text-4xl text-transparent bg-clip-text bg-gradient-to-r from-sky-200 via-white to-sky-200 font-extrabold flex justify-center p-4'>Advanced Business Solutions</span></h1>
+        <h1><span className='text-5xl text-transparent bg-clip-text bg-gradient-to-r from-sky-200 via-white to-sky-200 font-sans font-semibold flex justify-center p-4'>Advanced Business Solutions</span></h1>
         <img src='/images/XP Icon.png' className='w-full' ></img>
         <h1><span className='text-3xl text-transparent bg-clip-text bg-gradient-to-r from-sky-300 via-sky-500 to-sky-300 font-extrabold flex justify-center p-4'>Experience the Presence</span></h1>
         <Button variant="outline" className="block">Explore</Button>
